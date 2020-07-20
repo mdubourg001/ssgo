19e9a07ca461dffcdd2b90f7dc48a164c9734c08
maxime.dubourg <maxime.dubourg@ekino.com>
2020-07-20 23:02:50 +0200

    feat: removed export IBuildPageParams in place of BuildPage

--- mod.ts
+++ mod.ts
@@ -1,1 +1,22 @@
-export { IBuildPageParams, ISsgoBag } from "./src/types.ts";
+import {
+  ISsgoBag as SsgoBag,
+  IContextData,
+  IBuildPageOptions,
+} from "./src/types.ts";
+
+type BuildPage = (
+  /**
+   * The path of the template to use as page skeleton (relative to templates/ dir.)
+   */
+  template: string,
+  /**
+   * The contextual data used to build the page
+   */
+  data: IContextData,
+  /**
+   * Page build options
+   */
+  options: IBuildPageOptions
+) => void;
+
+export { BuildPage, SsgoBag };
